65c32c3f71e0259afd9cd16b3c0e3c09753a3be6
dimiter@abv.bg <dimiter@abv.bg>
2022-12-12 16:54:20 +0100

    MoreTests

---
+++ MachineBuildingFactoryTests/Controller/MaterialControllerTests.cs
@@ -0,0 +1,124 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MachineBuildingFactory.Areas.Management.Contracts;
+using MachineBuildingFactory.Areas.Management.Controllers;
+using MachineBuildingFactory.Areas.Management.Models;
+using MachineBuildingFactory.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MachineBuildingFactoryTests.Controller
+{
+    public class MaterialControllerTests
+    {
+        private readonly MaterialController materialController;
+        private readonly IMaterialService db;
+        private readonly IAssemblyService dbAssembly;
+
+        public MaterialControllerTests()
+        {
+            //Depen
+            db = A.Fake<IMaterialService>();
+            dbAssembly = A.Fake<IAssemblyService>();
+            materialController = new MaterialController(db, dbAssembly);
+        }
+
+        [Fact]
+        public async void MaterialController_AllMaterial_ReturnsViewResult()
+        {
+            //Arrange
+            var model = A.Fake<IEnumerable<MaterialViewModel>>();
+            A.CallTo(() => db.GetAllMaterialsAsync()).Returns(model);
+
+            //Act
+            var result = await materialController.AllMaterial();
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void MaterilaController_CreateNewMaterial_ReturnsViewResult()
+        {
+            //Arrange
+
+            //Act
+            var result = materialController.CreateNewMaterial();
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void MaterialController_CreateMaterial_ReturnsModel()
+        {
+            //Arrange
+            var model = A.Fake<CreateMaterialViewModel>();
+
+            //Act
+            var result = materialController.CreateNewMaterial(model);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void MaterialController_EditMaterial_ReturnsGetSuccess()
+        {
+            //Arrang
+            var id = 5;
+            var model = A.Fake<EditMaterialViewModel>();
+            A.CallTo(() => db.GetMaterialForEditAsync(id)).Returns(model);
+
+            //Act
+            var result = materialController.EditMaterial(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void MaterilaController_EditMaterial_ReturnsPostSuccess()
+        {
+            //Arrang
+            var model = A.Fake<EditMaterialViewModel>();
+
+            //Act
+            var result = materialController.EditMaterial(model);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public async void MaterialController_Details_ReturnsViewModel()
+        {
+            //Arrange
+            var id = 1;
+            var model = A.Fake<EditMaterialViewModel>();
+            A.CallTo(() => db.GetMaterialForEditAsync(id)).Returns(model);
+
+            //Act
+            var result = await materialController.Details(id);
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void MaterialController_Delete_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+
+            //Act
+            var result = materialController.Delete(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+    }
+}
---
+++ MachineBuildingFactoryTests/Controller/TypeOfProductionPartControllerTests.cs
@@ -0,0 +1,125 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MachineBuildingFactory.Areas.Management.Contracts;
+using MachineBuildingFactory.Areas.Management.Controllers;
+using MachineBuildingFactory.Areas.Management.Models;
+using MachineBuildingFactory.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MachineBuildingFactoryTests.Controller
+{
+    public class TypeOfProductionPartControllerTests
+    {
+        private readonly TypeOfProductionPartController typeOfProductionPartController;
+        private readonly ITypeOfProductionPartServices db;
+        private readonly IAssemblyService dbAssembly;
+
+        public TypeOfProductionPartControllerTests()
+        {
+            //Depen
+            db = A.Fake<ITypeOfProductionPartServices>();
+            dbAssembly = A.Fake<IAssemblyService>();
+            typeOfProductionPartController = new TypeOfProductionPartController(db, dbAssembly);
+        }
+
+        [Fact]
+        public async void TypeOfProductionPartController_AllTypeOfProductionPart_ReturnsViewResult()
+        {
+            //Arrange
+            var model = A.Fake<IEnumerable<TypeOfProductionPartViewModel>>();
+            A.CallTo(() => db.GetAllTypeOfProductionPartsAsync()).Returns(model);
+
+            //Act
+            var result = await typeOfProductionPartController.AllTypeOfProductionPart();
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void TypeOfProductionPart_CreateNewTypeOfProductionPart_ReturnsViewResult()
+        {
+            //Arrange
+
+            //Act
+            var result = typeOfProductionPartController.CreateNewTypeOfProductionPart();
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void TypeOfProductionPart_CreateNewTypeOfProductionPart_ReturnsModel()
+        {
+            //Arrange
+            var model = A.Fake<CreateTypeOfProductionPartViewModel>();
+
+            //Act
+            var result = typeOfProductionPartController.CreateNewTypeOfProductionPart(model);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void TypeOfProductionPartController_EditTypeOfProductionPart_ReturnsGetSuccess()
+        {
+            //Arrang
+            var id = 5;
+            var model = A.Fake<EditTypeOfProductionPartViewModel>();
+            A.CallTo(() => db.GetTypeOfProductionPartForEditAsync(id)).Returns(model);
+
+            //Act
+            var result = typeOfProductionPartController.EditTypeOfProductionPart(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void TypeOfProductionPartController_EditTypeOfProductionPart_ReturnsPostSuccess()
+        {
+            //Arrang
+            var model = A.Fake<EditTypeOfProductionPartViewModel>();
+
+            //Act
+            var result = typeOfProductionPartController.EditTypeOfProductionPart(model);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public async void TypeOfProductionPartController_Details_ReturnsViewModel()
+        {
+            //Arrange
+            var id = 1;
+            var model = A.Fake<EditTypeOfProductionPartViewModel>();
+            A.CallTo(() => db.GetTypeOfProductionPartForEditAsync(id)).Returns(model);
+
+            //Act
+            var result = await typeOfProductionPartController.Details(id);
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void TypeOfProductionPartController_Delete_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+
+            //Act
+            var result = typeOfProductionPartController.Delete(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+
+    }
+}
